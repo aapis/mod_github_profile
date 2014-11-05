b373c16d6d06dff2b9ca5b22bceeefa24a912188
Ryan Priebe <ryan@wearefree.ca>
2014-11-05 15:42:18 -0700

    more robust error handling, replaced a faulty if statement

--- helper.php
+++ helper.php
@@ -8,22 +8,28 @@
 			$username = $params->get("username", "aapis");
 			$auth = new GithubAPICredentials();
 
-			if($auth->id != "" || $auth->secret != ""){
-				$url = sprintf("https://api.github.com/users/%s?client_id=%s&client_secret=%s", $username, $auth->id, $auth->secret);
-			}else {
-				$url = sprintf("https://api.github.com/users/%s?access_token=%s", $username, $auth->token);
+			try {
+				if($auth->id != "" && $auth->secret != ""){
+					$url = sprintf("https://api.github.com/users/%s?client_id=%s&client_secret=%s", $username, $auth->id, $auth->secret);
+				}else if($auth->token != "") {
+					$url = sprintf("https://api.github.com/users/%s?access_token=%s", $username, $auth->token);
+				}else {
+					throw new Exception("You must have either an OAuth ID and secret OR a Github personal access token.");
+				}
+
+				$curl = curl_init();
+				curl_setopt_array($curl, array(
+					CURLOPT_RETURNTRANSFER => 1,
+					CURLOPT_URL => $url,
+					CURLOPT_USERAGENT => $username, //required by Github API guidelines
+				));
+
+				$response = curl_exec($curl);
+				
+				return json_decode($response);
+			}catch (Exception $e){
+				echo $e->getMessage();
 			}
-
-			$curl = curl_init();
-			curl_setopt_array($curl, array(
-				CURLOPT_RETURNTRANSFER => 1,
-				CURLOPT_URL => $url,
-				CURLOPT_USERAGENT => $username, //required by Github API guidelines
-			));
-
-			$response = curl_exec($curl);
-			
-			return json_decode($response);
 		}
 	}
 
